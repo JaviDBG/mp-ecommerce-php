d44e8c74e8d9c1895bacf72a826612c4eeaf525d
David <djbarriosgomez@gmail.com>
2020-08-29 16:53:49 -0500

    fix acces webhook

--- webhook.php
+++ webhook.php
@@ -1,4 +1,13 @@
 <?php
+$_SERVER['REQUEST_METHOD'];
+header("Access-Control-Allow-Origin: *");
+//header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS');
+header("Access-Control-Allow-Headers: X-Requested-With");
+//header('Content-Type: application/json');
+//header('Content-Type: text/html; charset=utf-8');
+header('P3P: CP="IDC DSP COR CURa ADMa OUR IND PHY ONL COM STA"');
+$_POST = json_decode(file_get_contents('php://input'), true);
 require __DIR__ .  '/vendor/autoload.php';
 MercadoPago\SDK::setAccessToken("APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181");
 $response;
@@ -27,6 +36,6 @@ file_put_contents(
     json_encode($response) . PHP_EOL,
     FILE_APPEND
   );
-http_response_code(200);
+http_response_code(404);
 
 ?>
